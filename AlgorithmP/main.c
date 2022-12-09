8b0a1cf27c02357b78afd333e81665c3c40f12b4
kimyj0530 <102145868+kimyj0530@users.noreply.github.com>
2022-12-09 11:43:21 +0900

    1

--- AlgorithmP/main.c
+++ AlgorithmP/main.c
@@ -1,15 +1,18 @@
 #include "stdio.h"
 #include "Windows.h"
 #include "stdlib.h"
+#include "Time.h"
+#define repeat 100
 
-int student[16] = {"1번", "2번", "3번", "4번", "5번", "6번", "7번", "8번", "9번", "10번", "11번", "12번", "13번", "14번", "15번", "16번"}
+int student[16] = { "1번", "2번", "3번", "4번", "5번", "6번", "7번", "8번", "9번", "10번", "11번", "12번", "13번", "14번", "15번", "16번" };
 double sec = 3;
-void timeBomb(double s)
+
+void timeBomb(double s, double random)
 {
     system("cls");
     if (s < 0) {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
-        printf("@ @@@@@@@@..@@ @@  @\n @@@..@@@@..@@@@@@  \n@@@@@@....  @@..@ @@\n......        ..@@@@\n@@..    퍼엉  ....@@\n@@@@..        ..@@@@\n@@......      ..@@@\n@@@@@@  ..  @@..  @@\n  @@@@..@@..@@@@@@@@\n @@@  @@@@@@@@  @  @");      //터지는 이미지 출력
+        printf("@ @@@@@@@@..@@ @@  @\n @@@..@@@@..@@@@@@  \n@@@@@@....  @@..@ @@\n......        ..@@@@\n@@..    %s  ....@@\n@@@@..        ..@@@@\n@@......      ..@@@\n@@@@@@  ..  @@..  @@\n  @@@@..@@..@@@@@@@@\n @@@  @@@@@@@@  @  @\n", student[1]);  
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
         return;
     }
@@ -22,16 +25,30 @@ void timeBomb(double s)
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
     printf("\t      │\n  └───────────────────┘");
     Sleep(100);
-    timeBomb(s - 0.1);
+    timeBomb(s - 0.1, random);
 }
 
-void number(100) {
+void repeat(1) {
+    int i, max;
+    int list[10] = { 0, };
 
+    srand((unsigned int)time(NULL));
+
+    for (i = 0; i < COUNT; i++) {
+        ++list[rand() % 10];
+    }
+    max = 0;
+    for (i = 1; i < 10; i++) {
+        if (list[i] > max)
+            max = i;
+    }
+    printf("가장 많이 나온수 = %d\n", max);
 }
 
 int main(void) {
-    timeBomb(sec);
-    number(100);
-    printf("%d", student);
+    int random = rand() % 16;
+    timeBomb(sec, random);
+    repeat(1)
+    printf("%s", student[1]);
     return 0;
 }
