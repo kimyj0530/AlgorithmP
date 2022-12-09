0299fc336f09661cf6a57e92ad7c7b30315a2f62
kimyj0530 <102145868+kimyj0530@users.noreply.github.com>
2022-12-09 09:43:51 +0900

    1

---
+++ AlgorithmP/main.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+#include "Windows.h"
+
+double sec = 3;
+void timeBomb(double s)
+{
+    system("cls");
+    if (s < 0) {
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+        printf("@ @@@@@@@@..@@ @@  @\n @@@..@@@@..@@@@@@  \n@@@@@@....  @@..@ @@\n......        ..@@@@\n@@..    퍼엉  ....@@\n@@@@..        ..@@@@\n@@......      ..@@@\n@@@@@@  ..  @@..  @@\n  @@@@..@@..@@@@@@@@\n @@@  @@@@@@@@  @  @");      //터지는 이미지 출력
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+        return;
+    }
+    printf("\n\n\n  ┌───────────────────┐\n  │\t  ");
+    if (s <= 0.5) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+    else if (s <= 1) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+    else if (s <= 2) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
+    else SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+    printf("%.1lf초", s);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+    printf("\t      │\n  └───────────────────┘");
+    Sleep(100);
+    timeBomb(s - 0.1);
+}
+
+
+int main(void) {
+    timeBomb(sec);
+    return 0; //어쩔티비
+}
